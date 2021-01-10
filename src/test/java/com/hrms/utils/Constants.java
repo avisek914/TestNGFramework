32a004fa1135a449ed8e0f59b69ef4dc4c477496
avisek914 <avisek914@gmail.com>
2021-01-09 23:41:09 -0500

    Framework Initial Setup

---
+++ src/test/java/com/hrms/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.hrms.utils;
+
+public class Constants {
+    public static final String CONFIGURATION_FILEPATH=System.getProperty("user.dir")+"/src/test/resources/Configs/configuration.properties";
+    public static final String TESTDATA_FILEPATH="";
+    public static final String SCREENSHOT_FILEPATH="C:\\Users\\Owner\\IdeaProjects\\TestNGFramework\\src\\test\\resources\\Configs\\configuration.properties";
+    public static final int IMPLICT_WAIT=10;
+}
